dd2ffc2c0d58544f8543600ea3a8114543bf428b
shawnxiao <shawnxiao@augmentum.com.cn>
2014-04-17 11:17:09 +0800

    db setting

--- microblog/Setting.js
+++ microblog/Setting.js
@@ -1,6 +1,6 @@
 module.exports = {
   cookieSecret: 'microblogbyvoid',
   db: 'microblog',
-  url: 'mongodb://127.0.0.1/microblog'
-  host: 'localhost',
+  url: 'mongodb://127.0.0.1/microblog',
+  host: 'localhost'
 };
